c5b9fc0a93f645cbab32e6e92f128097a683aaba
Vladimir <vladt89@yandex.ru>
2015-05-07 21:56:41 +0300

    Test Tina's polygon

---
+++ src/test/java/com/ekahau/pip/analyze/AnalyzeServiceIsPointInNonconvexPolygon.java
@@ -0,0 +1,81 @@
+package com.ekahau.pip.analyze;
+
+import com.ekahau.pip.common.Point;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * Test class for {@link AnalyzeService#isPointInPolygon(Point)} method.
+ *
+ * @author vladimir.tikhomirov
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-config.xml"})
+public class AnalyzeServiceIsPointInNonconvexPolygon {
+
+    public static final String POLYGON_POINTS = "src/test/resources/nonconvex/nonconvexPolygonTest.txt";
+    public static final String OUTSIDE_POINTS = "src/test/resources/nonconvex/outsidePoints.txt";
+    public static final String INSIDE_POINTS = "src/test/resources/nonconvex/insidePoints.txt";
+
+    @Autowired
+    AnalyzeServiceImpl analyzeService;
+    @Autowired
+    InputDataAnalyzer inputDataAnalyzer;
+
+    /**
+     * Create polygon based on the provided data.
+     * @throws Exception -
+     */
+    @Before
+    public void setUp() throws Exception {
+        final List<Point> polygon = inputDataAnalyzer.readFile(POLYGON_POINTS);
+        analyzeService.setPolygon(polygon);
+    }
+
+    /**
+     * Tests {@link AnalyzeService#isPointInPolygon(Point)} when the points belong to the polygon.
+     * @throws Exception -
+     */
+    @Test
+    public void testPointsInsidePolygon() throws Exception {
+        //SETUP SUT
+        List<Point> pointsInsidePolygon = inputDataAnalyzer.readFile(INSIDE_POINTS);
+        //EXERCISE & VERIFY
+        for (Point pointToVerify : pointsInsidePolygon) {
+            Assert.assertTrue(analyzeService.isPointInPolygon(pointToVerify));
+        }
+    }
+
+    /**
+     * Tests {@link AnalyzeService#isPointInPolygon(Point)} when the points do not belong to the polygon.
+     * @throws Exception -
+     */
+    @Test
+    public void testPointsOutsidePolygon() throws Exception {
+        //SETUP SUT
+        List<Point> pointsOutsideOfPolygon = inputDataAnalyzer.readFile(OUTSIDE_POINTS);
+
+        //EXERCISE & VERIFY
+        for (Point pointToVerify : pointsOutsideOfPolygon) {
+            Assert.assertFalse(analyzeService.isPointInPolygon(pointToVerify));
+        }
+    }
+
+//    @Test
+//    public void testPreparePolygon() throws Exception {
+//        //EXERCISE
+//        analyzeService.preparePolygon(new Point(2, 2));
+//
+//        //VERIFY
+//        Point mainPoint = analyzeService.getPolygon().get(0);
+//        Assert.assertEquals(1, mainPoint.getX(), GeometryServiceTest.DELTA);
+//        Assert.assertEquals(5, mainPoint.getY(), GeometryServiceTest.DELTA);
+//    }
+}
